006e484a70c9c0720fba6c75294fd889b21a809c
tsaebeht <piyush9620@outlook.com>
2017-01-02 10:20:29 +0530

    MADE SOME CHANGES 2

--- api/src/main/java/org/openmrs/Person.java
+++ api/src/main/java/org/openmrs/Person.java
@@ -20,6 +20,7 @@ import org.hibernate.search.annotations.FullTextFilterDef;
 import org.hibernate.search.annotations.FullTextFilterDefs;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.Resolution;
+import org.openmrs.api.db.hibernate.search.TermsFilterFactory;
 import org.openmrs.api.APIException;
 import org.openmrs.util.OpenmrsUtil;
 import org.springframework.util.StringUtils;
