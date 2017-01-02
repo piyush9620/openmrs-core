55b72211911f3c64c57429828560f580d97d8c94
tsaebeht <piyush9620@outlook.com>
2017-01-02 13:44:02 +0530

    MADE SOME CHANGES 5

--- api/src/main/java/org/openmrs/Person.java
+++ api/src/main/java/org/openmrs/Person.java
@@ -21,7 +21,6 @@ import org.hibernate.search.annotations.FullTextFilterDefs;
 import org.hibernate.search.annotations.Indexed;
 -import org.hibernate.search.annotations.Resolution;
 import org.openmrs.api.db.hibernate.search.TermsFilterFactory;
-import org.openmrs.api.APIException;
 import org.openmrs.util.OpenmrsUtil;
 import org.springframework.util.StringUtils;
 
@@ -310,7 +309,7 @@ public class Person extends BaseOpenmrsData {
 	/**
 	 * @param deathDate date of person's death
 	 */
-	public void setDeathDate (Date deathDate) throws APIException {
+	public void setDeathDate (Date deathDate) {
         if (deathDate != null) {
             this.dead = true;
         } else {
